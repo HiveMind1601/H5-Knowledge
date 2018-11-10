3ded71e46679d2689d9d21fa90a3629d654597be
sxx <azzlzzxz@gmail.com>
2018-11-10 14:08:57 +0800

    change

--- project/Vue/cmsapi/routes/upload.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const dataCtrl = require('../controllers/upload.controller');
-
-router.post('/uploadFile', dataCtrl.uploadFile);
-router.post('/list',dataCtrl.list);
-router.delete('/data/:id',dataCtrl.remove);
-
-module.exports = router;
